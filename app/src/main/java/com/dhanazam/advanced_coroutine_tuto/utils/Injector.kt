2178e52bd620d9f5112c51020c93cb26713801dd
Ramadhan <ramadanazam93@gmail.com>
2024-03-15 10:56:10 +0800

    add ViewModelFactoryProvider

--- app/src/main/java/com/dhanazam/advanced_coroutine_tuto/utils/Injector.kt
+++ app/src/main/java/com/dhanazam/advanced_coroutine_tuto/utils/Injector.kt
@@ -1,4 +1,37 @@
 package com.dhanazam.advanced_coroutine_tuto.utils
 
-class Injector {
+import android.content.Context
+import com.dhanazam.advanced_coroutine_tuto.NetworkService
+import com.dhanazam.advanced_coroutine_tuto.PlantRepository
+import com.dhanazam.advanced_coroutine_tuto.ui.PlantListViewModelFactory
+
+interface ViewModelFactoryProvider {
+    fun providePlantListViewModelFactory(context: Context): PlantListViewModelFactory
 }
+
+val Injector: ViewModelFactoryProvider
+    get() = currentInjector
+
+private object DefaultViewModelProvider: ViewModelFactoryProvider {
+
+    private fun getPlantRepository(context: Context): PlantRepository {
+        return PlantRepository.getInstance(
+            plantDao(context),
+            plantService()
+        )
+    }
+
+    private fun plantService() = NetworkService()
+
+    private fun plantDao(context: Context) =
+        AppDatabase.getInstance(context.applicationContext).plantDao()
+
+
+    override fun providePlantListViewModelFactory(context: Context): PlantListViewModelFactory {
+        val repository = getPlantRepository(context)
+        return PlantListViewModelFactory(repository)
+    }
+}
+
+@Volatile private var currentInjector: ViewModelFactoryProvider =
+    DefaultViewModelProvider
